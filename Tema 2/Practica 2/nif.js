e817dee8d88f9a752843eb8f1e33bb0fe87e185e
piternoob <pedroplazar94@gmail.com>
2017-10-02 23:02:35 +0200

    nif funciona

--- Tema 2/Practica 2/nif.js
+++ Tema 2/Practica 2/nif.js
@@ -4,38 +4,38 @@ function nif() {
   var numeros = "0123456789";
   var num = "";
   var contadorN = 0;
-  var cadenaN="";
+  var cadenaN = "";
   var primeroB;
   var control = "TRWAGMYFPDXBNJZSQVHLCKE";
   var controlB = false;
   var valor;
 
   cadena = cadena.toUpperCase();
-  if (cadena.length == 8) {
-    alert("Comprobar si tiene longitud 8");
+  alert("Comprobar si tiene longitud 9");
+  if (cadena.length == 9) {
+    alert("Tiene longitud 9")
     for (var i = 0; i < primero.length && !primeroB; i++) {
-      if (i == parseInt(primero.charAt(i))) {
+      if (cadena.charAt(0) == primero.charAt(i)) {
         primeroB = true;
       }
     }
     if (primeroB) {
       alert("El primer caracter es valido");
-      for (var i = 1; i < cadena.length - 2 && contadorN == (i - 1); i++) {
-        for (var j = 0; j < numeros.length && contadorN == i; j++) {
+      for (var i = 1; i < cadena.length - 1; i++) {
+        for (var j = 0; j < numeros.length; j++) {
           if (cadena.charAt(i) == numeros.charAt(j)) {
             contadorN++;
           }
         }
       }
-      if (contadorN == cadena.length - 2) {
+      if (contadorN == 7) {
         for (var i = 0; i < control.length && !controlB; i++) {
-          if (control.charAt(i) == cadena.charAt(length - 1)) {
+          if (control.charAt(i) == cadena.charAt(cadena.length - 1)) {
             controlB = true;
           }
         }
         if (controlB) {
-          alert("Comprobar control...");
-          controlB = false;
+          alert("Comprobar control");
           if (cadena.charAt(0) == "Y") {
             num += "1";
           } else if (cadena.charAt(0) == "Z") {
@@ -44,19 +44,15 @@ function nif() {
           for (var i = 1; i < cadena.length - 1; i++) {
             num += cadena.charAt(i);
           }
-
-          if (cadena.charAt(length - 1) == control.charAt(parseInt(num) % 23)) {
+          if (cadena.charAt(cadena.length - 1) == control.charAt(parseInt(num) % 23)) {
             document.write("Nif valido");
             valor = 1;
           } else {
             document.write("Fallo control");
             valor = 2;
           }
-
-
-
         } else {
-          document.write("Fallo control");
+          document.write("Fallo control1");
           valor = 2;
         }
       } else {
@@ -64,44 +60,88 @@ function nif() {
         valor = 0;
       }
     } else {
-      alert("Comprobar si es dni");
-      for (var i = 0; i < cadena.length; i++) {
-        for (var j = 0; j < numeros.length; j++) {
-          if (cadena.charAt(i) == numeros.charAt(j)) {
-            cadenaN+=cadena.charAt(i);
-          }
-        }
-      }
-      if (cadenaN.length == cadena.length && parseInt(cadenaN) >= 100000) {
-        document.write("Dni valido");
-        valor = 3;
-      } else {
-        document.write("Dni no valido "+cadenaN);
-        valor = 0;
-      }
-
+      document.write("Nif no valido ");
+      valor = 0;
     }
-  } else {
-    alert("Comprobar dni 6 7 numeros");
-
-
-    for (var i = 0; i < cadena.length && contadorN == i - 1; i++) {
-      for (var j = 0; j < numeros.length && contadorN == i; j++) {
+  } else if (cadena.length >= 6 || cadena.length <= 8) {
+    alert("Vemos si es un dni valido:");
+    for (var i = 0; i < cadena.length; i++) {
+      for (var j = 0; j < numeros.length; j++) {
         if (cadena.charAt(i) == numeros.charAt(j)) {
-          contadorN++;
+          cadenaN += cadena.charAt(i);
         }
       }
     }
-    if (contadorN == cadena.length && contadorN >= 100000) {
+    if (cadenaN.length == cadena.length) {
       document.write("Dni valido");
       valor = 3;
     } else {
-      document.write("Dni no valido");
+      document.write("Dni no valido ");
       valor = 0;
     }
-
-
-
+  } else {
+    document.write("Documento no valido ");
+    valor = 0;
   }
+}
 
+
+
+
+function cif() {
+  var cadena = prompt("Introduce un CIF:")
+  var control = 0;
+  //control=1 - Control es numero
+  //control=2 - Control es letra
+  var letrasN = "AHJUV";
+  var letrasL = "PQRSW";
+  var numeros = "0123456789";
+  var control;
+  var valor;
+
+  cadena = cadena.toUpperCase();
+  alert("Comprobamos si tiene longitud 9")
+  if (cadena.length == 9) {
+    for (var i = 0; i < letrasN.length && control == 0; i++) {
+      if (cadena.charAt(0) == letrasN.charAt(i)) {
+        control = 1;
+      } else if (cadena.charAt(0) == letrasL.charAt(i)) {
+        control = 2;
+      }
+    }
+    if (control == 1) {
+      control = false;
+      for (var i = 0; i < numeros.length && !control; i++) {
+        if (cadena.charAt(cadena.length - 1) == numeros.charAt(i)) {
+          control = true;
+        }
+      }
+      if (control) {
+        alert("Control valido");
+      } else {
+        document.write("CIF erroneo, control no valido ");
+        valor = 2;
+      }
+      alert("Control valido");
+    } else if (control == 2) {
+      control = true;
+      for (var i = 0; i < numeros.length && control; i++) {
+        if (cadena.charAt(cadena.length - 1) == numeros.charAt(i)) {
+          control = false;
+        }
+      }
+      if (control) {
+        alert("Control valido");
+      } else {
+        document.write("CIF erroneo, control no valido ");
+        valor = 2;
+      }
+    } else {
+      document.write("CIF no valido ");
+      valor = 0;
+    }
+  } else {
+    document.write("CIF no valido ");
+    valor = 0;
+  }
 }
